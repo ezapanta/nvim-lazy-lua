06d113e79211b0691e7f79398ee52fa1bc7569c5
ezapanta <eddy@Eddys-MacBook-Pro.local>
2025-10-06 17:56:01 -0500

    adjust for lspconfig v3 and fix fuzzy finder

--- lua/ed/plugins/telescope.lua
+++ lua/ed/plugins/telescope.lua
@@ -4,7 +4,11 @@ return {
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		-- fork off of nvim-telescope for apple sillicon
+<<<<<<< HEAD
 		{ "lucasfcosta//telescope-fzf-native.nvim", build = "make" },
+=======
+		{ "lucasfcosta/telescope-fzf-native.nvim", build = "make" },
+>>>>>>> b6bd0ea (adjust for lspconfig v3 and fix fuzzy finder)
 		"nvim-tree/nvim-web-devicons",
 	},
 	config = function()
@@ -23,9 +27,24 @@ return {
 					},
 				},
 			},
+<<<<<<< HEAD
 		})
 
 		telescope.load_extension("fzf")
+=======
+			extensions = {
+				fzf = {
+					fuzzy = true, -- false will only do exact matching
+					override_generic_sorter = true, -- override the generic sorter
+					override_file_sorter = true, -- override the file sorter
+					case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+					-- the default case_mode is "smart_case"
+				},
+			},
+		})
+
+		-- telescope.load_extension("fzf")
+>>>>>>> b6bd0ea (adjust for lspconfig v3 and fix fuzzy finder)
 
 		-- set keymaps
 		local keymap = vim.keymap -- for conciseness
